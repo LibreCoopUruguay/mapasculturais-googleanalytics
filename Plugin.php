ef9f4e00017567cf22919d010f56cab4cdfaeba1
Leo Germani <leogermani@hacklab.com.br>
2017-01-19 10:27:49 -0200

    versao 1.0

---
+++ Plugin.php
@@ -0,0 +1,33 @@
+<?php
+namespace GoogleAnalytics;
+
+use MapasCulturais\App;
+
+class Plugin extends \MapasCulturais\Plugin {
+    
+    public function _init() {
+        $app = App::i();
+        
+        $trackingID = $this->_config['tracking_id'];
+        
+        if (preg_match('/^UA-/', $trackingID) !== 1)
+            return;
+        
+        // Load JS & CSS
+        $app->hook('mapasculturais.head', function() use ($app, $trackingID) {
+            echo "<script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+              ga('create', '$trackingID', 'auto');
+              ga('send', 'pageview');
+
+            </script>";
+        }, 1000);
+        
+        
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mapasculturais-googleanalytics
 Google Analytics plugin for Mapas Culturais
+
+# Installation and activation
+
+Download and place this plugin inside you plugins folder located at protected/application/plugins and rename its folder to GoogleAnalytics.
+
+Edit your config.php to activate the plugin and inform you Tracking ID: 
+
+```
+
+'plugins' => [
+
+    //... other plugin you may have...
+    'mapasculturais-googleanalytics' => [
+        'namespace' => 'GoogleAnalytics',
+        'config' => [
+            'tracking_id' => 'UA-123456'
+        ]
+    ]
+],
+
+```PHP
+
+Your tracking is a string starting with "UA-". In order to get it, visit your Google Analytics panel. Go to the ADMIN tab, then under the "PROPERTY" column click on "Tracking info" and then "Tracking Code".
